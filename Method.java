41a68c866f692502624e0d49d1211eef344ec150
Naimul1 <islamnaimul1@gmail.com>
2016-12-15 21:21:57 -0500

    Add files via upload

---
+++ Method.java
@@ -0,0 +1,31 @@
+  // Welcome page method
+
+public class WelcomePage extends JPanel {É}
+
+// product
+
+public class Product extends JPanel implements ActionListener 
+	{..}
+
+// User Account 
+
+ public User(String id, String first, String last, String name, String pass )
+     {
+	
+     } 
+	 
+public Username( String name, String pass)
+     {
+        username = name;
+        password = pass;
+	 }
+//Create a new policy 
+ 
+public policy (String PolicyName, double PolicyPrice)
+    {
+      
+    }
+	
+// order
+Policy order(Policy id, String PolicyName, String IssueDate, String ExpiredDate, double PolicyPrice){} 
+
